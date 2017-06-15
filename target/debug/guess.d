44137edefb80816f0df87eadf5bf97a4bbdd46fd
WaltersDilemma <vater-adrian@gmx.de>
2017-06-15 14:08:32 +0200

    just recompiled trying some things out

--- target/debug/guess.d
+++ target/debug/guess.d
@@ -1,1 +1,1 @@
-C:\Users\adrix\rust\target\debug\guess.exe: C:\Users\adrix\rust\src\main.rs
+C:\Users\adrix\Documents\GitHub\guessing-game\target\debug\guess.exe: C:\Users\adrix\Documents\GitHub\guessing-game\src\main.rs
